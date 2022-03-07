c9d19e0f0df66a0bb0f3d9eb1d71b1f187398e1a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-07 12:09:25 -0500

    I forgor what I changed

--- src/main/java/studio/javezki/MathMain.java
+++ src/main/java/studio/javezki/MathMain.java
@@ -2,7 +2,6 @@ package studio.javezki;
 
 import javax.security.auth.login.LoginException;
 
-
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 
@@ -17,16 +16,16 @@ import studio.javezki.MathTopics.Topics;
  * Hello world!
  *
  */
-public class MathMain 
-{
+public class MathMain {
     private static EventWaiter waiter;
-    public static void main( String[] args )
-    {   
+
+    public static void main(String[] args) {
         Topics.addMathTopic(
 
-            new QuadraticTransformations()
+                new QuadraticTransformations()
 
-            );
+        );
+
         waiter = new EventWaiter();
 
         CommandClientBuilder builder = new CommandClientBuilder();
@@ -45,15 +44,14 @@ public class MathMain
 
         try {
             JDABuilder.createLight(DiscordKeys.getMathKey())
-            .addEventListeners(waiter, builder.build(), new OnHelp())
-            .build();
+                    .addEventListeners(waiter, builder.build(), new OnHelp())
+                    .build();
         } catch (LoginException e) {
             System.out.println("\u001B[35m" + "Error logging into bot! (Did you use the correct keys?)" + "\u001B[0m");
         }
     }
 
-    public static EventWaiter getWaiter()
-    {
+    public static EventWaiter getWaiter() {
         return waiter;
     }
 }
